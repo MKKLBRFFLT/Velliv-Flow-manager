ebd1f0ab1b7f89cf8103a5d8e304715cd1eba195
MKKLBRFFLT <mikkelreffelt@gmail.com>
2024-12-09 16:23:40 +0100

    minor stuff

--- src/app/flow/[flow]/page.tsx
+++ src/app/flow/[flow]/page.tsx
@@ -263,7 +263,7 @@ export default function FlowEditor() {
             onClick={() => handleDeletePage()}
             className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
           >
-            Delete page
+            Slet side
           </button>
         </div>
 
