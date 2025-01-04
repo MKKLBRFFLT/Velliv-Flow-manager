49fa07d9a3a23e4b780df32df78b201d60ad1367
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2025-01-04 18:29:54 +0100

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import LeftNavBar from "@/components/navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
