3b8f98611dc953c83921d86c9f1e0f5babb3b1c7
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2025-01-06 21:18:41 +0100

    fix for vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,11 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string; 
+const uri = process.env.MONGODB_URI as string;
 if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-
 let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
 
 declare global {
   // eslint-disable-next-line no-var
@@ -18,6 +16,7 @@ if (!global._mongoClientPromise) {
   client = new MongoClient(uri);
   global._mongoClientPromise = client.connect();
 }
-clientPromise = global._mongoClientPromise;
+
+const clientPromise: Promise<MongoClient> = global._mongoClientPromise;
 
 export default clientPromise;
